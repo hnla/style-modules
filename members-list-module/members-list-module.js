7fe30c5cf430835b9615f2d101ba7982fa8eb860
Hugo <hugo.ashmore@gmail.com>
2016-04-22 14:10:42 +0100

    Minor mods.

--- members-list-module/members-list-module.js
+++ members-list-module/members-list-module.js
@@ -5,19 +5,31 @@
 (function($){
 
 	var liItem = 'ul#members-list li .item-title';
+	var liActionCurrentUser = 'ul#members-list li.is-current-user .item';
 	var viewportWidth = $(window).width();
 
-	//stackoverflow.com/questions/6781031/use-jquery-css-to-find-the-tallest-of-all-elements
+
 	var itemTitleHeight = $('ul#members-list li .item-title').map(function() {
 		return $(this).height();
 	}).get();
 
 	var itemTitleMaxHeight = Math.max.apply(null, itemTitleHeight);
 
-	// Set all list elements .item div to min-height to equal box height's
+	var actionButtons = $('ul#members-list li .action').map(function() {
+		return $(this).height();
+	}).get();
+
+	var actionButtonHeight = Math.max.apply(null, actionButtons);
+
+	// Set all list elements .item-title div to min-height to equal box height's &
+	// add padding for empy action buttons container on current logged in user entry.
 
 	if(viewportWidth > '601') {
 		$( liItem ).css( {'min-height': itemTitleMaxHeight + 'px' });
+		$( liActionCurrentUser ).css({'padding-bottom': actionButtonHeight + 'px' });
+	}
+	if( $( 'ul#members-list li.is-current-user' ) ) {
+		$( 'ul#members-list li.is-current-user .action' ).hide();
 	}
 //	alert(viewportWidth);
 
